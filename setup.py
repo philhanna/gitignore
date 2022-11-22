b8636fbb81def6944169a234253633141b5b3cb3
Phil Hanna <ph1204@gmail.com>
2022-11-22 18:54:15 -0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gitignore',
+    version='1.0.0',
+    packages=['tests', 'gitignore'],
+    url='https://github.com/philhanna/gitignore',
+    license='MIT License',
+    author='Phil Hanna',
+    author_email='ph1204@gmail.com',
+    description='Creates basic .gitignore based on file type'
+)
